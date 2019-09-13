517a42d97040a4c5bb472fd39bc1a9472bb3ded3
Yash <yash_b@csu.fullerton.edu>
2019-09-12 20:51:36 -0700

    added objects

--- pong-game.py
+++ pong-game.py
@@ -5,11 +5,39 @@ import random
 
 pygame.init()
 main_clock = pygame.time.Clock()
-
-WINDOW_WIDTH, WINDOW_HEIGHT = 800, 400
-
+WINDOW_WIDTH, WINDOW_HEIGHT = 800, 500
 surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), 0, 32)
+pygame.display.set_caption("PONG GAME")
+clock = pygame.time.Clock()
 
+DOWN_LEFT = 'downleft'
+DOWN_RIGHT = 'downright'
+UP_LEFT = 'upleft'
+UP_RIGHT = 'upright'
+
+
+MOVE_SPEED = 4
 WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
 
-player_pong_single = pygame.Rect()
+player_pong_single = {'rect':pygame.Rect(300, 80, 50, 100), 'color':WHITE, 'dir':UP_RIGHT}
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.locals.QUT:
+            pygame.quit()
+            sys.exit()
+
+    # ToDO:
+    #  [ ] Game Logic -> Collision Detector -> Score
+    #  [ ] Sprites & Sound
+
+    surface.fill(BLACK)
+
+    pygame.draw.line(surface, WHITE, [399, 0], [399, 500], 3)
+    pygame.display.flip()
+    clock.tick(60)
+
+pygame.quit()
+
+
